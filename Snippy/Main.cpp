fb7342eb59aaacb64a246dc6c6608f8216e77d7d
Jack Pilley <boxedvideos@gmail.com>
2022-08-03 09:47:34 -0700

    Experimented with hotkeys

--- Snippy/Main.cpp
+++ Snippy/Main.cpp
@@ -18,10 +18,46 @@ int WINAPI main(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, in
 	return wWinMain(hInstance, hPrevInstance, pCmdLine, nCmdShow);
 }
 
+LRESULT CALLBACK MainWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
-	PipWindow window(hInstance);
-	PipWindow window2(hInstance);
+	const wchar_t CLASS_NAME[] = L"MainClass";
+
+	WNDCLASS wc = {};
+	wc.lpfnWndProc = MainWindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+
+	RegisterClass(&wc);
+
+	HWND hwnd = CreateWindowEx(
+		0,
+		CLASS_NAME,
+		L"Main Window",
+		0,
+
+		0,
+		0,
+		0,
+		0,
+
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	if (hwnd == 0)
+	{
+		std::cout << "Error creating window: " << GetLastError() << '\n';
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	// Register hotkey Ctrl-Shift-W. Using C would make more sense but it's taken by gyazo.
+	if (RegisterHotKey(hwnd, 1, MOD_CONTROL | MOD_SHIFT | MOD_NOREPEAT, 0x57))
+		std::cout << "Registered hotkey\n";
 
 	MSG msg = {};
 	while (GetMessage(&msg, NULL, 0, 0) > 0)
@@ -31,4 +67,17 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 	}
 
 	return 0;
+}
+
+LRESULT CALLBACK MainWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+
+	switch (uMsg)
+	{
+	case WM_HOTKEY:
+		std::cout << "Pressed\n";
+		return 0;
+	default:
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
 }
